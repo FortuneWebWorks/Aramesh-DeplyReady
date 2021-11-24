801f9922f8f800acad53792a0e8332a8b3ca7ef7
Badie <badie.farzandi@gmail.com>
2021-11-24 16:28:47 +0800

    :bug: IntegrationTable minor bugs fixed

--- laravel-core/resources/js/src/components/IntegrationTable.js
+++ laravel-core/resources/js/src/components/IntegrationTable.js
@@ -28,7 +28,7 @@ const IntegrationTable = (props) => {
             </tr>
               <tr>
                 {props.integration.map((integration, index) => {
-                  (index <= 0 && <td>{integration.role}</td>)
+                  (index <= 0 && <td key={`parent-parent-role-${index}`}>{integration.role}</td>)
                 })}
               </tr>
             </>
@@ -49,9 +49,9 @@ const IntegrationTable = (props) => {
               {props.integration.map((integration, index) => {
                 let parentNumber = 0;
                 if(props.parents.length > 1)  parentNumber = 1;
-                return (index >= parentNumber && index < props.children.length * props.parents.length + parentNumber && <tr>
-                  <td  key={`parent-children-role-${index}`}>{integration.role}</td>
-                  <td key={`parent-children-data-${index}`}>{integration.integration}</td>
+                return (index >= parentNumber && index < props.children.length * props.parents.length + parentNumber && <tr key={`parent-children-role-${index}`}>
+                  <td>{integration.role}</td>
+                  <td>{integration.integration}</td>
                 </tr>)
               })}
             </>
@@ -72,9 +72,9 @@ const IntegrationTable = (props) => {
               {props.integration.map((integration, index) => {
                 let parentNumber = 0;
                 if(props.parents.length > 1)  parentNumber = 1;
-                return (index >= props.children.length * props.parents.length + parentNumber && index < props.children.length * props.parents.length + parentNumber + ((props.children.length * (props.children.length - 1)) / 2) && <tr>
-                  <td  key={`parent-children-role-${index}`}>{integration.role}</td>
-                  <td key={`parent-children-data-${index}`}>{integration.integration}</td>
+                return (index >= props.children.length * props.parents.length + parentNumber && index < props.children.length * props.parents.length + parentNumber + ((props.children.length * (props.children.length - 1)) / 2) && <tr key={`children-children-role-${index}`}>
+                  <td>{integration.role}</td>
+                  <td>{integration.integration}</td>
                 </tr>)
               })}
             </>
