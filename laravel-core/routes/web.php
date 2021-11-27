eabed83a6fd4ce64c292a7a44961c67096f6c215
Badie <badie.farzandi@gmail.com>
2021-11-27 19:11:48 +0800

    :zap: Minor changes for deploymnet

--- laravel-core/routes/web.php
+++ laravel-core/routes/web.php
@@ -95,116 +95,116 @@ Route::prefix('/test/{person}/{member}')->middleware('auth')->group(function() {
 
 Route::get('/result/{role}', [ResultController::class, 'results'])->middleware('auth')->name('result');
 
-Route::get('/data', function () {
+// Route::get('/data', function () {
 
-    $shiraz = App\Models\City::create([
-        'name' => 'شیراز'
-    ]);
+//     $shiraz = App\Models\City::create([
+//         'name' => 'شیراز'
+//     ]);
 
-    $marvdasht = App\Models\City::create([
-        'name' => 'مرودشت'
-    ]);
+//     $marvdasht = App\Models\City::create([
+//         'name' => 'مرودشت'
+//     ]);
 
-    $aramesh = $shiraz->clinics()->create([
-        'name' => 'مرکز آرامش',
-        'province' => 'فارس',
-        'address' => 'خیابان فلسطین - بین هدایت و معدل - طبقه فوقانی اتومبیل بهادری',
-        'phone-number' => '07132343435',
-    ]);
+//     $aramesh = $shiraz->clinics()->create([
+//         'name' => 'مرکز آرامش',
+//         'province' => 'فارس',
+//         'address' => 'خیابان فلسطین - بین هدایت و معدل - طبقه فوقانی اتومبیل بهادری',
+//         'phone-number' => '07132343435',
+//     ]);
 
-    $aramesh->practitioners()->create([
-        'name' => 'دکتر سیامک سامانی',
-        'code' => '342',
-        'phone-number' => '9173150780'
-    ]);
+//     $aramesh->practitioners()->create([
+//         'name' => 'دکتر سیامک سامانی',
+//         'code' => '342',
+//         'phone-number' => '9173150780'
+//     ]);
 
-    $aramesh->practitioners()->create([
-        'name' => 'تست',
-        'code' => '343',
-        'phone-number' => '9332782917'
-    ]);
+//     $aramesh->practitioners()->create([
+//         'name' => 'تست',
+//         'code' => '343',
+//         'phone-number' => '9332782917'
+//     ]);
 
-    $aramesh->practitioners()->create([
-        'name' => 'تست',
-        'code' => '344',
-        'phone-number' => '9172253052'
-    ]);
+//     $aramesh->practitioners()->create([
+//         'name' => 'تست',
+//         'code' => '344',
+//         'phone-number' => '9172253052'
+//     ]);
     
-    $aramesh->practitioners()->create([
-        'name' => 'doctor',
-        'code' => '345',
-        'phone-number' => '9052409115'
-    ]);
+//     $aramesh->practitioners()->create([
+//         'name' => 'doctor',
+//         'code' => '345',
+//         'phone-number' => '9052409115'
+//     ]);
     
 
-    $aramesh->practitioners()->create([
-        'name' => 'دکتر نادره سهرابی',
-        'code' => '3565',
-        'phone-number' => '9176307130'
-    ]);
+//     $aramesh->practitioners()->create([
+//         'name' => 'دکتر نادره سهرابی',
+//         'code' => '3565',
+//         'phone-number' => '9176307130'
+//     ]);
 
-    $hamgam = $shiraz->clinics()->create([
-        'name' => 'مرکز مشاوره همگام',
-        'province' => 'فارس',
-        'address' => 'خیابان اردیبهشت - حد فاصل ملاصدرا و فلسطین - ساختمان آذر - طبقه پنجم - واحد 10',
-        'phone-number' => '07132319099',
-    ]);
+//     $hamgam = $shiraz->clinics()->create([
+//         'name' => 'مرکز مشاوره همگام',
+//         'province' => 'فارس',
+//         'address' => 'خیابان اردیبهشت - حد فاصل ملاصدرا و فلسطین - ساختمان آذر - طبقه پنجم - واحد 10',
+//         'phone-number' => '07132319099',
+//     ]);
 
-    $hamgam->practitioners()->create([
-        'name' => 'لیلا رشیدبیگی',
-        'code' => '755',
-        'phone-number' => '9909772805'
-    ]);
+//     $hamgam->practitioners()->create([
+//         'name' => 'لیلا رشیدبیگی',
+//         'code' => '755',
+//         'phone-number' => '9909772805'
+//     ]);
 
-    $raheZendegi = $shiraz->clinics()->create([
-        'name' => 'مرکز مشاوره راه زندگی',
-        'province' => 'فارس',
-        'address' => 'خیابان ملاصدرا - نبش خیابان حکیمی - ساختمان حکیم - طبقه چهارم',
-        'phone-number' => '07132346043',
-    ]);
+//     $raheZendegi = $shiraz->clinics()->create([
+//         'name' => 'مرکز مشاوره راه زندگی',
+//         'province' => 'فارس',
+//         'address' => 'خیابان ملاصدرا - نبش خیابان حکیمی - ساختمان حکیم - طبقه چهارم',
+//         'phone-number' => '07132346043',
+//     ]);
 
-    $raheZendegi->practitioners()->create([
-        'name' => 'سعید رحیمی خواه',
-        'code' => '1730',
-        'phone-number' => '9173017886'
-    ]);
+//     $raheZendegi->practitioners()->create([
+//         'name' => 'سعید رحیمی خواه',
+//         'code' => '1730',
+//         'phone-number' => '9173017886'
+//     ]);
 
-    $zendegiAgahane = $shiraz->clinics()->create([
-        'name' => 'مرکز مشاوره زندگی آگاهانه',
-        'province' => 'فارس',
-        'address' => 'بیست متری سینما سعدی - نبش معدل - ساختمان سال - طبقه 4 - واحد 402',
-        'phone-number' => '07132304774',
-    ]);
+//     $zendegiAgahane = $shiraz->clinics()->create([
+//         'name' => 'مرکز مشاوره زندگی آگاهانه',
+//         'province' => 'فارس',
+//         'address' => 'بیست متری سینما سعدی - نبش معدل - ساختمان سال - طبقه 4 - واحد 402',
+//         'phone-number' => '07132304774',
+//     ]);
 
-    $zendegiAgahane->practitioners()->create([
-        'name' => 'بهاره زارع',
-        'code' => '7916',
-        'phone-number' => '9308137354'
-    ]);
+//     $zendegiAgahane->practitioners()->create([
+//         'name' => 'بهاره زارع',
+//         'code' => '7916',
+//         'phone-number' => '9308137354'
+//     ]);
 
-    $aban = $shiraz->clinics()->create([
-        'name' => 'مرکز مشاوره آبان',
-        'province' => 'فارس',
-        'address' => 'خیابان ارم - نبش سروناز - پلاک 2 - کلینیک آبان',
-        'phone-number' => '07132291737',
-    ]);
+//     $aban = $shiraz->clinics()->create([
+//         'name' => 'مرکز مشاوره آبان',
+//         'province' => 'فارس',
+//         'address' => 'خیابان ارم - نبش سروناز - پلاک 2 - کلینیک آبان',
+//         'phone-number' => '07132291737',
+//     ]);
 
-    $aban->practitioners()->create([
-        'name' => 'فاطمه حسنی',
-        'code' => '3487',
-        'phone-number' => '9173131259'
-    ]);
+//     $aban->practitioners()->create([
+//         'name' => 'فاطمه حسنی',
+//         'code' => '3487',
+//         'phone-number' => '9173131259'
+//     ]);
 
-    $peyman = $marvdasht->clinics()->create([
-        'name' => 'مرکز پیمان',
-        'province' => 'فارس',
-        'address' => 'خیابان سعدی - نبش خیابان فرهنگ - ساختمان فرهنگ - طبقه اول - مرکز مشاوره امید',
-        'phone-number' => '07132291737',
-    ]);
+//     $peyman = $marvdasht->clinics()->create([
+//         'name' => 'مرکز پیمان',
+//         'province' => 'فارس',
+//         'address' => 'خیابان سعدی - نبش خیابان فرهنگ - ساختمان فرهنگ - طبقه اول - مرکز مشاوره امید',
+//         'phone-number' => '07132291737',
+//     ]);
 
-    $peyman->practitioners()->create([
-        'name' => 'امید زارع نژاد',
-        'code' => '2388',
-        'phone-number' => '9019783535'
-    ]);
-});
+//     $peyman->practitioners()->create([
+//         'name' => 'امید زارع نژاد',
+//         'code' => '2388',
+//         'phone-number' => '9019783535'
+//     ]);
+// });
